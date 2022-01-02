e657d66a5fcfc1509ef2a2ba883e205a670ac0f6
fenngZ <zf950801@gmail.com>
2022-01-02 23:06:40 +0800

    feat(server): listObjects add url in response

--- server/src/main/java/io/openbytes/odi/application/DatasetFileService.java
+++ server/src/main/java/io/openbytes/odi/application/DatasetFileService.java
@@ -1,13 +1,12 @@
 package io.openbytes.odi.application;
 
 import cn.hutool.core.util.StrUtil;
-import io.openbytes.odi.domain.DatasetFile;
+import io.openbytes.odi.domain.storage.ListFilesResponse;
 import io.openbytes.odi.domain.storage.Storage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Optional;
 
 @Slf4j(topic = "metricsLog")
 @Service
@@ -19,14 +18,14 @@ public class DatasetFileService {
         this.storage = storage;
     }
 
-    public List<DatasetFile> list(String datasetName, String marker, int maxKeys) {
+    public Optional<ListFilesResponse> list(String datasetName, String marker, int maxKeys) {
         // todo check dataset
         if (StrUtil.isEmpty(datasetName)) {
-            return Collections.emptyList();
+            return Optional.of(ListFilesResponse.emptyResponse());
         }
 
         // append suffix
         datasetName = StrUtil.addSuffixIfNot(datasetName, "/");
-        return storage.ListDatasetFiles(datasetName, marker, maxKeys);
+        return Optional.of(storage.ListDatasetFiles(datasetName, marker, maxKeys));
     }
 }
--- server/src/main/java/io/openbytes/odi/domain/storage/Storage.java
+++ server/src/main/java/io/openbytes/odi/domain/storage/Storage.java
@@ -1,16 +1,32 @@
 package io.openbytes.odi.domain.storage;
 
-import io.openbytes.odi.domain.DatasetFile;
-
 import java.util.List;
+import java.util.Map;
 
 public interface Storage {
     /**
      * list odi dataset files
-     * @param prefix prefix you want to query, eg: abc/
+     *
+     * @param prefix  prefix you want to query, eg: abc/
      * @param marker
      * @param maxKeys
      * @return todo we should update resp, add isTruncated info...
      */
-    List<DatasetFile> ListDatasetFiles(String prefix, String marker, int maxKeys);
+    ListFilesResponse ListDatasetFiles(String prefix, String marker, int maxKeys);
+
+    /**
+     * get file url
+     *
+     * @param key
+     * @return url
+     */
+    String GetUrl(String key);
+
+    /**
+     * get file urls
+     *
+     * @param keys
+     * @return
+     */
+    Map<String, String> GetUrls(List<String> keys);
 }
