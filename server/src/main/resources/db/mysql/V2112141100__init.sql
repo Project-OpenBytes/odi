037b0fa2fa6bbd27da121671b7c4480d5b21aaac
fenngZ <zf950801@gmail.com>
2022-01-05 15:37:56 +0800

    style(all): add Apache-License-2.0 in file header

--- server/src/main/resources/db/mysql/V2112141100__init.sql
+++ server/src/main/resources/db/mysql/V2112141100__init.sql
@@ -1,19 +1,62 @@
+/*
+ * Copyright 2021 The OpenBytes Team. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 CREATE TABLE IF NOT EXISTS `dataset`
 (
-    `id` VARCHAR(128) NOT NULL,
-    `name` VARCHAR(512) NOT NULL,
-    `homepage` VARCHAR(1024) NOT NULL,
+    `id` VARCHAR
+(
+    128
+) NOT NULL,
+    `name` VARCHAR
+(
+    512
+) NOT NULL,
+    `homepage` VARCHAR
+(
+    1024
+) NOT NULL,
     `description` TEXT NOT NULL,
-    `readme_link` VARCHAR(1024) NOT NULL,
-    `created_at`  DATETIME(3),
-    `updated_at`  DATETIME(3),
+    `readme_link` VARCHAR
+(
+    1024
+) NOT NULL,
+    `created_at` DATETIME
+(
+    3
+),
+    `updated_at` DATETIME
+(
+    3
+),
     `owner_name` TEXT,
-    `creator_user_id` VARCHAR(128) ,
-    `creator_org_id` VARCHAR(128),
+    `creator_user_id` VARCHAR
+(
+    128
+) ,
+    `creator_org_id` VARCHAR
+(
+    128
+),
     `view_count` INT,
     `star_count` INT,
     `download_count` INT,
-    PRIMARY KEY (`id`) USING BTREE
+    PRIMARY KEY
+(
+    `id`
+) USING BTREE
     )
     ENGINE = InnoDB
     CHARACTER SET = utf8mb4
