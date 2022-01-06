9884582ca24be26f03a1387a1bf55f22664e87d6
fenngZ <zf950801@gmail.com>
2022-01-06 14:59:41 +0800

    test(server): add codecov integration, add datasetService.getByName test

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # OpenBytes Dataset Index
-This is the repository contains everything you can find in datasets.openBytes.io, including backend http server &amp; frontend &amp; cli &amp; sdk source code etc.
 
+This is the repository contains everything you can find in datasets.openBytes.io, including backend http server &amp;
+frontend &amp; cli &amp; sdk source code etc.
 
 [![Build](https://github.com/Project-OpenBytes/odi/actions/workflows/java_ci.yml/badge.svg)](https://github.com/Project-OpenBytes/odi/actions/workflows/java_ci.yml)
+[![CircleCI](https://circleci.com/gh/Project-OpenBytes/odi/tree/main.svg?style=svg)](https://circleci.com/gh/Project-OpenBytes/odi/tree/main)
+[![codecov](https://codecov.io/gh/Project-OpenBytes/odi/branch/main/graph/badge.svg?token=TV76OEYHE7)](https://codecov.io/gh/Project-OpenBytes/odi)
 [![Build](https://github.com/Project-OpenBytes/odi/actions/workflows/python_sdk_ci.yml/badge.svg)](https://github.com/Project-OpenBytes/odi/actions/workflows/python_sdk_ci.yml)
 [![Coverage Status](https://coveralls.io/repos/github/Project-OpenBytes/odi/badge.svg)](https://coveralls.io/github/Project-OpenBytes/odi)
---
+++ server/src/test/java/io/openbytes/odi/application/DatasetServiceTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2021 The OpenBytes Team. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openbytes.odi.application;
+
+import io.openbytes.odi.domain.Dataset;
+import io.openbytes.odi.domain.common.HttpURL;
+import io.openbytes.odi.domain.repository.DatasetRepository;
+import io.openbytes.odi.interfaces.vo.DatasetVO;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DatasetServiceTest {
+
+    @InjectMocks
+    DatasetService datasetService;
+
+    @Mock
+    DatasetRepository datasetRepository;
+
+    @Test
+    public void testGetByName() {
+        Dataset dataset1 = new Dataset("test_dataset", new HttpURL("http://baidu.com"),
+                "desc", new HttpURL("http://baidu.com"), Instant.now(), Instant.now(), "ownerName", "1", "1");
+
+        when(datasetRepository.getByName(dataset1.getName())).then(invocation -> Optional.of(dataset1));
+
+        Optional<DatasetVO> vo = Assertions.assertDoesNotThrow(() -> datasetService.getByName(dataset1.getName()));
+        Assertions.assertEquals(dataset1.getName(), vo.get().name);
+        Assertions.assertEquals(dataset1.getHomepage().getUrl(), vo.get().homepage);
+    }
+
+    @Test
+    public void testGetById() {
+        
+    }
+}
